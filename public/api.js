7a879990d28c2705f8d0818e2bba7b6cd5173471
Dewitt Buckingham <dajoker29@gmail.com>
2024-12-06 12:20:16 -0800

    Add more styling

--- public/api.js
+++ public/api.js
@@ -23,10 +23,37 @@ async function displayRecent() {
     console.log(recent);
 }
 
+async function displayCurrent() {
+    const url = endpoint + '/recent';
+    const options = { method: 'GET' }
+
+    const response = await fetch(url, options);
+    const json = await response.json();
+    const current = json[0];
+
+    console.log(current);
+
+    const currentContainer = document.querySelector('#current');
+
+    const fahrenheit = cToFDegrees(current.body.data.values.temperature).toFixed(1);
+    const text = `Temperature: ${fahrenheit}°\nHumidity: ${current.body.data.values.humidity}%\nWind Speed: ${current.body.data.values.windSpeed}mph\nPrecipitation: ${current.body.data.values.precipitationProbability}%`
+
+    const values = text.split('\n');
+    console.log(values);
+
+    values.forEach(value => {
+        const divContainer = document.createElement('div');
+        divContainer.textContent = value;
+        currentContainer.appendChild(divContainer);
+    })
+
+}
+
 function cToFDegrees(celcius) {
     return celcius * 1.8 + 32;
 }
 
 export default function api() {
+    displayCurrent();
     displayRecent();
 }
